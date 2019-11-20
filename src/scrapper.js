83a551d713317d9b372861f8c0a5083788e88641
Clément Besse <clement.besse@gmail.com>
2019-11-20 23:30:38 +0100

    Fix can't start app on server

--- src/scrapper.js
+++ src/scrapper.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer')
 
 const scrapper = async (url, cb) => {
-  const browser = await puppeteer.launch({ headless: false })
+  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'], headless: true })
   const page = await browser.newPage()
 
   await page.goto(url)
